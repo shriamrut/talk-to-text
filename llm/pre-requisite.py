da428d8dd06c2ec0cdaaf3946cb23f35912748d7
Shriamrut V <shriamrutvenkatesh@gmail.com>
2024-03-18 08:11:22 +0530

    Add logger and also update the model used
    Add logger and also update the model used to a smaller model (160M)

--- llm/pre-requisite.py
+++ llm/pre-requisite.py
@@ -1,7 +1,8 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2", 
+tokenizer = AutoTokenizer.from_pretrained("Felladrin/Sheared-Pythia-160m-WebGLM-QA", 
                                           trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2",
+model = AutoModelForCausalLM.from_pretrained("Felladrin/Sheared-Pythia-160m-WebGLM-QA",
                                             trust_remote_code=True)
+#TODO Add code to download chroma model for embedding- /root/.cache/chroma/onnx_models/all-MiniLM-L6-v2/onnx.tar.gz
--- llm/service.py
+++ llm/service.py
@@ -9,18 +9,23 @@ class LLMService:
         tokenizer_model_name = os.environ["HF_TOKENIZER_MODEL"]
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.device = device
+        self.logger = logging.getLogger(__name__)
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
     
     def query(self, relevant_texts, query, max_new_tokens = 500, temperature = 0.7, do_sample = True):
-        context = "[" + ",".join(relevant_texts) + "]" 
-        prompt = self._get_prompt(context, query)
-        logging.debug(f"Prompt: {prompt}")
+        prompt = self._form_prompt(relevant_texts=relevant_texts, query = query)
+        self.logger.debug(f"Prompt: {prompt}")
         input_ids = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
         output = self.model.generate(input_ids, do_sample=do_sample, temperature=temperature, max_new_tokens=max_new_tokens)
         generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
         return generated_text
 
-    def _get_prompt(self, context, query):
-        prompt = f'You are a Large Language Chat Model. User: context: {context} \n query: {query} \n Constraint: Please answer the prompt based on the context provided. And if the answer is not present in the context, then answer only if you are sure. \n Model: '
-        return prompt
+    def _form_prompt(self, relevant_texts, query):
+        context = "<|references|> \n"
+        for i, relevant_text in enumerate(relevant_texts):
+            context += f"[{i+1}] {relevant_text}\n" 
+        context += "</s>"
+        context += f"<|question|> {query} </s>\n"
+        context += "<|answer|> "
+        return context
